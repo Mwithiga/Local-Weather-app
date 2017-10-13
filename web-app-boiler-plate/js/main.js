10b740a6a2ae5f610ae9e157a5d5cdcaa8435165
Mwithiga <mwithigajames@outlook.com>
2017-10-13 20:27:08 +0300

    Setting up the boiler plate

---
+++ web-app-boiler-plate/js/main.js
@@ -0,0 +1,35 @@
+  /* ================================================================
+      Web App Boilerplate
+    ==================================================================*/
+    require.config({
+      paths: {
+        'jquery':'libs/jquery-1.10.1.min',
+        'underscore': 'libs/underscore',
+        'backbone': 'libs/backbone',
+        'bootstrap': 'libs/bootstrap'
+      },
+      shim: {
+        'underscore' : {
+          exports: '_'
+        },
+        'backbone': {
+          deps: [
+            'underscore',
+            'jquery'
+            ],
+            exports: 'Backbone'
+        }
+        'bootstrap': {
+          deps: [
+            'jquery'
+          ]
+        }
+      }
+    });
+
+    require([
+        'backbone'
+      ], function (Backbone) {
+        'use strict';
+        console.log(Backbone);
+      })
